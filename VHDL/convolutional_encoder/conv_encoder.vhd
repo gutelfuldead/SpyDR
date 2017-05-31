2bc1df72a54d1c4cfe96245a8c381db91e34ac17
jgutel <gutel@hcs.ufl.edu>
2017-05-31 13:55:26 -0700

    updated conv_encoder.vhd

--- VHDL/convolutional_encoder/conv_encoder.vhd
+++ VHDL/convolutional_encoder/conv_encoder.vhd
@@ -14,25 +14,32 @@ entity conv_encoder is
 end conv_encoder;
 
 architecture behavioral of conv_encoder is
-  signal sig_shift_reg_0, sig_shift_reg_1 : std_logic := '0';
-  begin
+	
+	signal sig_shift_reg_0, sig_shift_reg_1 : std_logic := '0';
+	
+	begin
+    
     conv_encoder : process (clk, rst)
-      variable count : integer range -1 to din'length := din'length - 1;
+    	variable count : integer range -1 to din'length := din'length - 1;
     begin
-      if (rst = '1') then
-        count := din'length - 1; -- proceed through SLV in reverse order
-        sig_shift_reg_0 <= '0';
-        sig_shift_reg_1 <= '0';
-        dout <= (others => '0');
-        done <= '0';
-      elsif (count < 0) then
-        done <= '1';
-      elsif (rising_edge(clk) and en = '1') then -- proceed through encoder
-        sig_shift_reg_0 <= din(count);
-        sig_shift_reg_1 <= sig_shift_reg_0;
-        dout(count*2+1)   <= din(count) xor sig_shift_reg_0 xor sig_shift_reg_1; -- 111 binary
-        dout(count*2)     <= din(count) xor sig_shift_reg_1;       -- 101 binary
-        count := count - 1;
-      end if;
+	if (rst = '1') then -- asynch reset
+    	count := din'length - 1; -- proceed through SLV in reverse order
+    	sig_shift_reg_0 <= '0';
+    	sig_shift_reg_1 <= '0';
+    	dout <= (others => '0');
+    	done <= '0';
+	    elsif(rising_edge(clk)) then
+	    	if(en = '1') then
+	  	    	if (count < 0) then
+	       			done <= '1';
+	       		else -- proceed through encoder
+	           		sig_shift_reg_0 <= din(count);
+	           		sig_shift_reg_1 <= sig_shift_reg_0;
+	           		dout(count*2+1)   <= din(count) xor sig_shift_reg_0 xor sig_shift_reg_1; -- 111 binary
+	           		dout(count*2)     <= din(count) xor sig_shift_reg_1;       -- 101 binary
+	           		count := count - 1;
+	       		end if;
+  		end if;
+ 	end if;
     end process conv_encoder;
 end behavioral;
